38ac2ae15e4b69daf69127aea97d6b59f087e42c
Samuele Facenda <samuele.facenda@buonarroti.tn.it>
2022-10-31 12:13:38 +0100

    aggiornato file sql

--- src/com/socket/provaDb.sql
+++ src/com/socket/provaDb.sql
@@ -1,40 +1,30 @@
 -- database: 'samudb'
 -- username: 'samu'
 
--- create a table for storing usernames and passwords, and a table
--- for storing the user's data: the timestamp of creation of the record,
--- a 100 char string called 'label' and a 100 char string called 'file'
--- and the user id
-
 CREATE TABLE users (
-    id serial PRIMARY KEY,
-    username varchar(50) NOT NULL UNIQUE CHECK (username <> ''),
-    password varchar(50) NOT NULL CHECK (password <> '')
+                       id serial PRIMARY KEY,
+                       username varchar(50) NOT NULL UNIQUE CHECK (username <> ''),
+                       password varchar(97) NOT NULL CHECK (password <> '')
 );
 
 CREATE TABLE data (
-    id serial PRIMARY KEY,
-    user_id integer NOT NULL,
-    label varchar(100) NOT NULL,
-    file varchar(100),
-    created_at timestamp NOT NULL
+                      id serial PRIMARY KEY,
+                      user_id integer NOT NULL,
+                      label varchar(100) NOT NULL,
+                      file varchar(100),
+                      created_at timestamp NOT NULL
 );
 
-INSERT INTO users (username, password) VALUES ('samu', 'samu');
-
-ALTER TABLE users
-    ALTER COLUMN password TYPE varchar(97),
-    ALTER COLUMN password SET NOT NULL ;
 
 CREATE TABLE tokens (
-    id serial PRIMARY KEY,
-    user_id integer NOT NULL,
-    token varchar(110) NOT NULL,
-    created_at timestamp NOT NULL
+                        id serial PRIMARY KEY,
+                        user_id integer NOT NULL,
+                        token varchar(110) NOT NULL,
+                        expires_at timestamp NOT NULL
 );
 
-ALTER TABLE tokens
-    ADD COLUMN expires_at timestamp NOT NULL;
 
-ALTER TABLE tokens
-    DROP COLUMN created_at;
+INSERT INTO users
+(username, password)
+VALUES
+('samu', encode(sha256(sha256('samu')),'base64'));
